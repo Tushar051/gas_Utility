933070243c6399fdc51c2f57a328ac66db696312
Tushar <tusharkedar110@gmail.com>
2025-01-19 13:13:02 +0530

    .

--- request/urls.py
+++ request/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.home, name='home'),  # Home page (customer view)
     path('submit/', views.submit_request, name='submit_request'),  # Form submission page
